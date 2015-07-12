7f65de04c86ad59e81cb678d3a4f9dd1fa783842
Michael Aufreiter <michael@substance.io>
2015-07-13 01:48:58 +0200

    Cleanup.

--- server.js
+++ server.js
@@ -3,17 +3,11 @@ var express = require('express');
 var path = require('path');
 var Substance = require("substance");
 var fs = require('fs');
-var bodyParser = require('body-parser');
-
 var app = express();
 var port = process.env.PORT || 5000;
-
 var browserify = require("browserify");
 var babelify = require("babelify");
 
-// use body parser so we can get info from POST and/or URL parameters
-app.use(bodyParser.json({limit: '3mb'}));
-app.use(bodyParser.urlencoded({ extended: true }));
 
 // use static server
 app.use(express.static(path.join(__dirname, "public")));
@@ -22,17 +16,9 @@ app.use(express.static(path.join(__dirname, "public")));
 // --------------------
 
 app.get('/app.js', function (req, res, next) {
-  // var startTime = Date.now();
   browserify({ debug: true, cache: false })
-    // .transform(babelify.configure({ only: [
-    //   path.join(__dirname, 'src'),
-    //   path.join(__dirname, 'node_modules', 'substance-ui')
-    // ]}))
     .transform(babelify)
     .add(path.join(__dirname, "src", "app.js"))
-    // .on('file', function(file, id, parent) {
-    //   console.log('### ', (Date.now() - startTime));
-    // })
     .bundle()
     .on('error', function(err, data){
       console.error(err.message);
